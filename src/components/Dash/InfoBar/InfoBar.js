538553ae4f50c4be4961933b19ca58fbb17676de
Maximilian118 <maxcrosby118@gmail.com>
2020-12-04 18:39:16 +0300

    InfoBar Populated

--- src/components/Dash/InfoBar/InfoBar.js
+++ src/components/Dash/InfoBar/InfoBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './_InfoBar.scss'
+import moment from 'moment'
 
-const InfoBar = () => <div className="infobar"/>
+const InfoBar = () => 
+  <div className="infobar">
+    <h6>{moment().subtract(1, 'year').format("D MMM YYYY")} - {moment().format("D MMM YYYY")}</h6>
+  </div>
 
 export default InfoBar
