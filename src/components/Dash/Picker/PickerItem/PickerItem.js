081d515296460211b3e382840ece2f2123cb936c
Maximilian118 <maxcrosby118@gmail.com>
2020-12-11 15:25:33 +0300

    hover > bg

--- src/components/Dash/Picker/PickerItem/PickerItem.js
+++ src/components/Dash/Picker/PickerItem/PickerItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './_PickerItem.scss'
 
 const PickerItem = ({ symbol }) => 
-  <div className={`pickeritem ${symbol.y && `hover`}`}>
+  <div className={`pickeritem ${symbol.y && `bg`}`}>
     <div className="symbol-colour-circle" style={{ background: symbol.colour }}/>
     <p className="symbol">{symbol.symbol}</p><p>{symbol.candles ? Number(symbol.candles.data.slice(-1)[0].y).toFixed(2) : symbol.data.y}</p>
   </div>
