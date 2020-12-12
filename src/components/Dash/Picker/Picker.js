3ee7f82959130d07c241b73d2f1aae08965c6a17
Maximilian118 <maxcrosby118@gmail.com>
2020-12-12 14:12:44 +0400

    Picker Hover Chevrons

--- src/components/Dash/Picker/Picker.js
+++ src/components/Dash/Picker/Picker.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './_Picker.scss'
 import PickerItem from './PickerItem'
 
-const Picker = ({ user }) =>
+const Picker = ({ user }) => 
   <div className="picker">
-    {user.symbols.map(symbol => <PickerItem key={symbol.symbol} symbol={symbol}/>)}
+    {
+      user.symbols.map((symbol, i) => 
+        <PickerItem 
+        key={i} 
+        symbol={symbol} 
+        i={i} 
+        total={user.symbols.length}
+      />)
+    }
   </div>
 
 export default Picker
--- src/components/Dash/Picker/PickerItem/PickerItem.js
+++ src/components/Dash/Picker/PickerItem/PickerItem.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './_PickerItem.scss'
+import { ChevronUp, ChevronDown } from 'react-feather'
 
-const PickerItem = ({ symbol }) => 
+const PickerItem = ({ symbol, i, total }) => 
   <div className={`pickeritem ${symbol.y && `bg`}`}>
     <div className="symbol-colour-circle" style={{ background: symbol.colour }}/>
-    <p className="symbol">{symbol.symbol}</p><p>{symbol.candles ? Number(symbol.candles.data.slice(-1)[0].y).toFixed(2) : symbol.data.y}</p>
+    <p className="symbol">{symbol.symbol}</p>
+    <p className="last-price">{symbol.candles ? Number(symbol.candles.data.slice(-1)[0].y).toFixed(2) : symbol.data.y}</p>
+    {!symbol.y && <div className="btns">
+      {i !== total - 1 && <ChevronDown style={{ marginRight: i === 0 ? 18 : 0 }}/>}
+      {i !== 0 && <ChevronUp/>}
+    </div>}
   </div>
 
 export default PickerItem
