25a70ad0fb179242af9fa5fbb81878d9f0201cd4
Maximilian118 <maxcrosby118@gmail.com>
2021-04-30 17:40:49 +0100

    Exchange with Arrow

--- src/components/Dash/InfoBar/InfoBar.js
+++ src/components/Dash/InfoBar/InfoBar.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_InfoBar.scss'
 import moment from 'moment'
+import { ChevronUp } from 'react-feather'
 
-const InfoBar = ({ exchange }) => 
-  <div className="infobar">
-    {exchange.display && exchange.data ? 
-      <h6>{`Exchange: ${exchange.exchange}`}</h6> :
-      <h6>{moment().subtract(1, 'year').format("D MMM YYYY")} - {moment().format("D MMM YYYY")}</h6>
-    }
-  </div>
+const InfoBar = ({ exchange }) => {
+  const [ flipSVG, setflipSVG ] = useState(false)
+
+  return (
+    <div className="infobar">
+      {exchange.display && exchange.data ?
+        <div className="exchange-select" onClick={() => setflipSVG(!flipSVG)}>
+          <h6>{`Exchange: ${exchange.exchange}`}</h6>
+          <ChevronUp className={flipSVG ? "flipSVG" : "undifined"}/>
+        </div> 
+        :
+        <h6>{moment().subtract(1, 'year').format("D MMM YYYY")} - {moment().format("D MMM YYYY")}</h6>
+      }
+    </div>
+  )
+}
 
 export default InfoBar
