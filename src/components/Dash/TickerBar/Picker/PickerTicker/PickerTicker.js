35ecc08f7852d4c1639ad248a26f44ce11d72dbe
Maximilian118 <maxcrosby118@gmail.com>
2021-05-04 17:11:48 +0200

    Chevrons animation

--- src/components/Dash/TickerBar/Picker/PickerTicker/PickerTicker.js
+++ src/components/Dash/TickerBar/Picker/PickerTicker/PickerTicker.js
@@ -7,8 +7,11 @@ const PickerTicker = ({ symbol, exchange, setExchange }) =>
     className={`picker-ticker ${exchange === symbol && `exchange-selected`}`} 
     onClick={() => setExchange && setExchange(symbol)}
   >
+    {/* <div className="symbol-colour-circle"/> */}
     <p className="symbol">{symbol}</p>
     <ChevronRight/>
+    <ChevronRight className={`chevron-2 ${exchange === symbol && `move-chevron-2`}`}/>
+    <ChevronRight className={`chevron-3 ${exchange === symbol && `move-chevron-3`}`}/>
   </div>
 
 export default PickerTicker
