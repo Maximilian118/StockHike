1f697545282a9025b0f445539ff78b5f9a9289f4
Maximilian118 <maxcrosby118@gmail.com>
2021-05-07 11:55:11 +0200

    Picker type & exchange

--- src/components/Dash/TickerBar/Picker/Picker.js
+++ src/components/Dash/TickerBar/Picker/Picker.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './_Picker.scss'
 import PickerTicker from './PickerTicker'
 import { finnHubExchanges } from '../../../../shared/utility'
 
 const Picker = () => {
+  const [ type, setType ] = useState("")
   const [ exchange, setExchange ] = useState("")
+  
+  console.log(type, exchange)
 
-  let exchangeArr = null
-  Object.entries(finnHubExchanges).forEach(type => {
-    if (exchange === type[0]) {
-      exchangeArr = type[1]
+  let exchangeArr = []
+  Object.entries(finnHubExchanges).forEach(typ => {
+    if (type === typ[0]) {
+      exchangeArr = typ[1]
     }
   })
+
+  useEffect(() => {
+    if (type !== "" && !finnHubExchanges[type].includes(exchange)) {
+      setExchange("")
+    }
+  }, [type]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="picker">
@@ -20,15 +29,17 @@ const Picker = () => {
           <PickerTicker 
           key={i} 
           symbol={symbol}
-          exchange={exchange}
-          setExchange={setExchange}
+          type={type}
+          setType={setType}
         />)}
       </div>
       <div className="picker-col">
-        {exchangeArr && exchangeArr.map((symbol, i) => 
+        {exchangeArr.map((symbol, i) => 
           <PickerTicker
           key={i}
           symbol={symbol}
+          exchange={exchange}
+          setExchange={setExchange}
         />)}
       </div>
       <div className="picker-col">
--- src/components/Dash/TickerBar/Picker/PickerTicker/PickerTicker.js
+++ src/components/Dash/TickerBar/Picker/PickerTicker/PickerTicker.js
@@ -2,16 +2,29 @@ import React from 'react'
 import './_PickerTicker.scss'
 import { ChevronRight } from 'react-feather'
 
-const PickerTicker = ({ symbol, exchange, setExchange }) => 
-  <div 
-    className={`picker-ticker ${exchange === symbol && `exchange-selected`}`} 
-    onClick={() => setExchange && setExchange(symbol)}
-  >
-    {/* <div className="symbol-colour-circle"/> */}
-    <p className="symbol">{symbol}</p>
-    <ChevronRight/>
-    <ChevronRight className={`chevron-2 ${exchange === symbol && `move-chevron-2`}`}/>
-    <ChevronRight className={`chevron-3 ${exchange === symbol && `move-chevron-3`}`}/>
-  </div>
+const PickerTicker = ({ symbol, type, setType, exchange, setExchange }) => {
+  let isClicked = false
+  
+  if (type === symbol) {
+    isClicked = true
+  } else if (exchange === symbol) {
+    isClicked = true
+  }
+  
+  return (
+    <div 
+      className={`picker-ticker ${isClicked && `exchange-selected`}`} 
+      onClick={() => {
+        setType && setType(symbol)
+        setExchange && setExchange(symbol)
+      }}
+    >
+      <p className="symbol">{symbol}</p>
+      <ChevronRight/>
+      <ChevronRight className={`chevron-2 ${isClicked && `move-chevron-2`}`}/>
+      <ChevronRight className={`chevron-3 ${isClicked && `move-chevron-3`}`}/>
+    </div>
+  )
+} 
 
 export default PickerTicker
